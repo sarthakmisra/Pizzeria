f4dc122e1214e760ab1473488f0898207ce7eca1
sarthakmisra <78964203+sarthakmisra@users.noreply.github.com>
2021-09-02 11:42:47 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,11 @@
+pizzasarray = []
+function display() 
+ namesarray.push("Chicken-Tandoori-pizza")
+ namesarray.push("Deluxe-Veggie-pizza")
+ namesarray.push("Paneer-Tikka-Pizza")
+ namesarray.push("Veg-ExtraVaganza-Pizza")
+ namesarray.push("Veg-Margherita-pizza")
+ namesarray.push("Veg-Supreme-Pizza")
+
+ document.getElementById("pizzassss").innerHTML = namesarray;
+ 
